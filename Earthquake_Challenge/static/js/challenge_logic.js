aec5e3ea146d3ea0fd4265f6404effc2d5a509b0
khanh703 <khanh703@gmail.com>
2021-12-19 11:35:00 -0700

    Moved JSON file into static folder

--- Earthquake_Challenge/static/js/challenge_logic.js
+++ Earthquake_Challenge/static/js/challenge_logic.js
@@ -164,7 +164,7 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
 
     // 3. Use d3.json to make a call to get our Tectonic Plate geoJSON data.
     //d3.json("//github.com/fraxen/tectonicplates/blob/master/GeoJSON/PB2002_boundaries.json").then(function(data) {
-    d3.json("PB2002_boundaries.json").then(function(data) {
+    d3.json("static/PB2002_boundaries.json").then(function(data) {
       L.geoJSON(data, {
         style: techtonicStyleInfo
       }).addTo(techtonicPlates)
@@ -184,33 +184,3 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
     majorEarthquakes.addTo(map);
 
 });
-
-
-
-
-/*
-
-d3.json("https://github.com/fraxen/tectonicplates/blob/master/GeoJSON/PB2002_boundaries.json").then(function(data) {
-  L.geoJSON(data, {
-    style: myStyle,
-    onEachFeature: function(feature, layer){
-      let popup = L.popup().setContent("<h2>"+feature.properties.place +", " +feature.properties.mag+ "</h2>");
-      layer.bindPopup(popup)
-     }
-  }).addTo(map);
-});
-
-
-// Creating a GeoJSON layer with the retrieved data.
-  L.geoJSON(data, {
-    pointToLayer: function(feature, latlng) {
-        console.log(data);
-        let popup = L.popup().setContent("<h2>Location: "+feature.properties.place +",<h2><h3>Magnitude: " +feature.properties.mag+ "</h3>");
-            return L.circleMarker(latlng)
-            .bindPopup(popup);
-    },
-    style: styleInfo
-  }).addTo(earthquakes);
-
-
-*/
